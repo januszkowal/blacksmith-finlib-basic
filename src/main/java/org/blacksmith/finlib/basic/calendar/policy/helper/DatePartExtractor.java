0f0bb3d13f48781c88c1fa1f7512c7d25a6b4662
januszkowal <jkowalrz@gmail.com>
2021-05-25 10:32:42 +0200

    Removed calendar

--- src/main/java/org/blacksmith/finlib/basic/calendar/policy/helper/DatePartExtractor.java
+++
@@ -1,7 +0,0 @@
-package org.blacksmith.finlib.basic.calendar.policy.helper;
-
-import java.time.LocalDate;
-
-public interface DatePartExtractor<U> {
-  U extract(LocalDate date);
-}
--- src/main/java/org/blacksmith/finlib/basic/calendar/policy/helper/DatePartProvider.java
+++
@@ -1,5 +0,0 @@
-package org.blacksmith.finlib.basic.calendar.policy.helper;
-
-public interface DatePartProvider<U> {
-  boolean contains(U key);
-}
